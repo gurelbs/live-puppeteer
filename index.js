b695ff3f4fdf99bb630338db9c09092f9c960015
gurelbs <guri240@gmail.com>
2021-08-13 08:18:25 +0300

    updated

--- index.js
+++ index.js
@@ -18,9 +18,9 @@ app.use(cors())
 app.use(express.json())
 
 if (process.env.NODE_ENV) {
-	app.use(express.static(path.resolve(process.cwd(), 'client/build')))
+	app.use(express.static('~/client/build'))
 	app.get('*', (req, res) => {
-		res.sendFile(path.resolve(process.cwd(), 'client/build/index.html'))
+		res.sendFile(path.resolve('~/client/build/index.html'))
 	})
 }
 
