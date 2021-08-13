621a53bcbf2dbd9426ad8f8a27c526012b3f264b
gurelbs <guri240@gmail.com>
2021-08-13 16:09:17 +0300

    updated

--- index.js
+++ index.js
@@ -16,7 +16,6 @@ const PORT = process.env.PORT || 5000
 const app = express()
 app.use(cors())
 app.use(express.json())
-console.log(process.cwd());
 if (process.env.NODE_ENV) {
 	app.use(express.static(path.resolve(process.cwd(), 'client/build')))
 	app.get('*', (req, res) => {
