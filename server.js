73529a9b6581810a9fc71b5430402770f5af0ba3
gurelbs <guri240@gmail.com>
2021-08-13 17:55:09 +0300

    updated

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
-if (proccess.env.NODE_ENV === "production"){
+if (process.env.NODE_ENV === "production"){
 	app.use(express.static('client/build'));
 	app.get('*', function (req, res) {
 	 res.sendFile(path.resolve(__dirname, 'client/build', 'index.html'));
