6dfe632ef10754d420c2f821732490323359d06b
gurelbs <guri240@gmail.com>
2021-08-13 16:27:08 +0300

    updated

--- server/index.js
+++ server/index.js
@@ -16,9 +16,9 @@ const PORT = process.env.PORT || 5000
 const app = express()
 app.use(cors())
 app.use(express.json())
-app.use(express.static(path.resolve(__dirname, '../client/build')));
-app.get('*', function(req, res) {
-	res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+app.use(express.static(path.resolve(__dirname, './../client/build')));
+app.get('/*', function(req, res) {
+	res.sendFile(path.resolve(__dirname, './../client/build', 'index.html'));
 });
 	// .post('/translate', async (req, res) => {
 	// 	try {
