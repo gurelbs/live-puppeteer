fb6afa2292ff938a7ea71d25041d58521f81cc0c
gurelbs <guri240@gmail.com>
2021-08-13 17:14:58 +0300

    updated

--- server/index.js
+++ server/index.js
@@ -19,7 +19,7 @@ app.use(express.json())
 app.use(express.static(path.join(__dirname, './../client/build')));
 console.log(path.resolve(__dirname, './../client/build'));
 app.get('/', function(req, res) {
-	res.sendFile('index.html', { root: path.join(__dirname, '../client/build') });
+	res.sendFile(path.resolve('client', 'build', 'index.html') );
 });
 	// .post('/translate', async (req, res) => {
 	// 	try {
