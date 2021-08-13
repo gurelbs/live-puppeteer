91b29e52981aa370da17cc4101e259f5c855e3ae
gurelbs <guri240@gmail.com>
2021-08-13 17:39:55 +0300

    updated

--- server.js
+++ server.js
@@ -16,8 +16,8 @@ const PORT = process.env.PORT || 5000
 const app = express()
 app.use(cors())
 app.use(express.json())
-app.use(express.static(__dirname + 'client/build'));
-app.use(favicon(__dirname + 'client/build/favicon.ico'));
+app.use(express.static(__dirname + '/client/build'));
+app.use(favicon(__dirname + '/client/build/favicon.ico'));
 app.get('/*', function (req, res) {  
 	res.setHeader('Cache-Control', 'no-store, no-cache, must-revalidate, proxy-revalidate, max-age=0')                           
 	res.sendFile(path.join(__dirname + 'client/build/index.html'));
