9937cc70fadc1edd6ebeb59f04d015dc3f773e5e
gurelbs <guri240@gmail.com>
2021-08-13 17:52:41 +0300

    updated

--- server.js
+++ server.js
@@ -16,11 +16,15 @@ const PORT = process.env.PORT || 5000
 const app = express()
 app.use(cors())
 app.use(express.json())
-app.use(express.static(path.join(__dirname, 'build')));
 
-app.get('*', function (req, res) {
- res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
+if (proccess.env.NODE_ENV === "production"){
+	app.use(express.static('client/build'));
+	app.get('*', function (req, res) {
+	 res.sendFile(path.resolve(__dirname, 'client/build', 'index.html'));
+	});
+
+}
+
 	// .post('/translate', async (req, res) => {
 	// 	try {
 	// 		let answer = await answers.news('קורונה')
